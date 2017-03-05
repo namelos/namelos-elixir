14a353fb2faa2ad50868b26324a937b3cdb97a94
Fred Wang <namelos@outlook.com>
2017-03-05 15:58:48 +0800

    restore react index

--- web/static/js/index.js
+++ web/static/js/index.js
@@ -1,29 +1,25 @@
-// import { AppContainer } from 'react-hot-loader'
-// import React from 'react'
-// import ReactDOM from 'react-dom'
-// import Root from './config/Root'
+import { AppContainer } from 'react-hot-loader'
+import React from 'react'
+import ReactDOM from 'react-dom'
+import Root from './config/Root'
 
 const rootEl = document.getElementById('app')
 
-const Elm = require('../elm/main.elm')
+ReactDOM.render(
+  <AppContainer>
+    <Root />
+  </AppContainer>,
+  rootEl
+)
 
-Elm.Main.embed(rootEl)
-
-// ReactDOM.render(
-//   <AppContainer>
-//     <Root />
-//   </AppContainer>,
-//   rootEl
-// )
-
-// if (module.hot) {
-//   module.hot.accept('./config/Root', () => {
-//     const NextRoot = require('./config/Root').default;
-//     ReactDOM.render(
-//       <AppContainer>
-//         <NextRoot />
-//       </AppContainer>,
-//       rootEl
-//     )
-//   })
-// }
+if (module.hot) {
+  module.hot.accept('./config/Root', () => {
+    const NextRoot = require('./config/Root').default;
+    ReactDOM.render(
+      <AppContainer>
+        <NextRoot />
+      </AppContainer>,
+      rootEl
+    )
+  })
+}
