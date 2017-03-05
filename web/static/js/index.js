8188d057bc149e45cae9f914de80c724268fa62c
Fred Wang <namelos@outlook.com>
2017-03-05 16:03:17 +0800

    disable javascript for now

--- web/static/js/index.js
+++ web/static/js/index.js
@@ -1,25 +1,25 @@
-import { AppContainer } from 'react-hot-loader'
-import React from 'react'
-import ReactDOM from 'react-dom'
-import Root from './config/Root'
+// import { AppContainer } from 'react-hot-loader'
+// import React from 'react'
+// import ReactDOM from 'react-dom'
+// import Root from './config/Root'
 
-const rootEl = document.getElementById('app')
+// const rootEl = document.getElementById('app')
 
-ReactDOM.render(
-  <AppContainer>
-    <Root />
-  </AppContainer>,
-  rootEl
-)
+// ReactDOM.render(
+//   <AppContainer>
+//     <Root />
+//   </AppContainer>,
+//   rootEl
+// )
 
-if (module.hot) {
-  module.hot.accept('./config/Root', () => {
-    const NextRoot = require('./config/Root').default;
-    ReactDOM.render(
-      <AppContainer>
-        <NextRoot />
-      </AppContainer>,
-      rootEl
-    )
-  })
-}
+// if (module.hot) {
+//   module.hot.accept('./config/Root', () => {
+//     const NextRoot = require('./config/Root').default;
+//     ReactDOM.render(
+//       <AppContainer>
+//         <NextRoot />
+//       </AppContainer>,
+//       rootEl
+//     )
+//   })
+// }
