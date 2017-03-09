7149b8c1887785c2e26ae0f7264a37ea54df8069
Fred Wang <namelos@outlook.com>
2017-03-09 22:29:29 +0800

    send registration request

--- web/static/js/view/account/Registration.js
+++ web/static/js/view/account/Registration.js
@@ -1,14 +1,30 @@
 import React from 'react'
+import { bindActionCreators } from 'redux'
+import { connect } from 'react-redux'
 import { Field, reduxForm } from 'redux-form'
+import { register } from '../../model/account'
 
-export const Registration = ({ handleSubmit }) => <form>
-  <p>Registration</p>
-  <label htmlFor="email">Email</label>
-  <Field name="email" component="input" />
-  <label htmlFor="password">Email</label>
-  <Field name="password" component="input" />
-</form>
+const mapState = ({ account }) => ({ user: account.user })
+const mapDispatch = dispatch => bindActionCreators({ register }, dispatch)
 
-export default reduxForm({
-  form: 'registration'
-})(Registration)
+let RegistrationForm = ({ handleSubmit, register }) =>
+  <form onSubmit={handleSubmit}>
+    <p>Registration</p>
+
+    <label htmlFor="email">Email</label>
+    <Field name="email" component="input"/>
+
+    <label htmlFor="password">Email</label>
+    <Field name="password" component="input"/>
+
+    <button type="submit">Submit</button>
+  </form>
+
+RegistrationForm = reduxForm({ form: 'registration' })(RegistrationForm)
+
+const Registration = ({ register }) => {
+  const handleSubmit = user => register(user)
+  return <RegistrationForm onSubmit={handleSubmit} />
+}
+
+export default connect(mapState, mapDispatch)(Registration)
