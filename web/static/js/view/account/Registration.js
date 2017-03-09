9472de6e873b59ad2be1f01b8e3f1e69ea8107a5
Fred Wang <namelos@outlook.com>
2017-03-09 21:31:27 +0800

    add registration form layout

--- web/static/js/view/account/Registration.js
+++ web/static/js/view/account/Registration.js
@@ -1,5 +1,14 @@
 import React from 'react'
+import { Field, reduxForm } from 'redux-form'
 
-export const Registration = () => <div>
-  Registration
-</div>
+export const Registration = ({ handleSubmit }) => <form>
+  <p>Registration</p>
+  <label htmlFor="email">Email</label>
+  <Field name="email" component="input" />
+  <label htmlFor="password">Email</label>
+  <Field name="password" component="input" />
+</form>
+
+export default reduxForm({
+  form: 'registration'
+})(Registration)
