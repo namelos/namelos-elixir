01622dec50d6fd8f74a1485033dc059faa9c67d9
Fred Wang <namelos@outlook.com>
2017-03-11 19:15:46 +0800

    add aphrodite

--- web/static/js/src/view/App.js
+++ web/static/js/src/view/App.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Link } from 'react-router'
+import { StyleSheet, css } from 'aphrodite'
 
 const links = [
   { to: "/", text: "Home" },
@@ -12,10 +13,19 @@ export const App = ({ children }) => <div>
   { children }
 </div>
 
-const RouteLinks = ({ links }) => <ul>
+const RouteLinks = ({ links }) => <ul className={css(styles.nav)}>
   { links.map(link => <RouteLink link={link} key={link.to} />) }
 </ul>
 
-const RouteLink = ({ link }) => <li>
+const RouteLink = ({ link }) => <li className={css(styles.navItem)}>
   <Link to={link.to}>{ link.text }</Link>
 </li>
+
+const styles = StyleSheet.create({
+  nav: {
+    display: 'flex'
+  },
+  navItem: {
+    flex: 1
+  }
+})
